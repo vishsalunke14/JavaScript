8440dd1b9a5c66a6152327a728f4c9615f63bb02
vishsalunke14 <142652220+vishsalunke14@users.noreply.github.com>
2024-01-12 12:27:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)  //we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Vishal")
+
+let name = "Vishal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 5
+// bigint 
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Vishal")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
+
+
