6349751d484a673ffa711be9691ab4baba80058b
vishsalunke14 <142652220+vishsalunke14@users.noreply.github.com>
2024-01-19 15:43:06 +0000

    function scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+
+
+var c =300
+let a =100
+
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("inner a", a);
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c)
